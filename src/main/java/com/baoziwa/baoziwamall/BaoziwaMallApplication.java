a15317447920202ca844f619ab35144d642a4618
baoziwa <baoziwaka@qq.com>
2021-05-23 18:47:21 +0800

    the first commit

---
+++ src/main/java/com/baoziwa/baoziwamall/BaoziwaMallApplication.java
@@ -0,0 +1,13 @@
+package com.baoziwa.baoziwamall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoziwaMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaoziwaMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baoziwa/baoziwamall/BaoziwaMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baoziwa.baoziwamall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoziwaMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
